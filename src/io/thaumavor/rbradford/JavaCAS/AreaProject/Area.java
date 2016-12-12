90c3390219fdc3f5df1b7f1bad728190a7a90832
Ryan Bradford <ryanbradford333@gmail.com>
2016-12-12 16:55:43 -0500

    Fixed a problem with graphing undefined functions.

--- src/io/thaumavor/rbradford/JavaCAS/AreaProject/Area.java
+++ src/io/thaumavor/rbradford/JavaCAS/AreaProject/Area.java
@@ -14,14 +14,13 @@ public class Area {
 		JFrame frame = new JFrame();
 		frame.setBounds(0, 0, 1920, 1080);
 		frame.setVisible(true);
-		DifferentialFunction f = new DifferentialFunction("5*sin(y)+x", 0, 0);
+		Function f = new Function("ln(x)");
 		Function g = new Function("cos(x)");		
 		ArrayList<Function> array = new ArrayList<Function>();
 		array.add(f);
 		array.add(g);
 		GraphDisplay display = new GraphDisplay(array, 1, 2, 0, 1);
 		frame.add(display);
-		display.repaint();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
--- src/io/thaumavor/rbradford/JavaCAS/AreaProject/GraphDisplay.java
+++ src/io/thaumavor/rbradford/JavaCAS/AreaProject/GraphDisplay.java
@@ -24,13 +24,13 @@ public class GraphDisplay extends JPanel {
 	int graphHeight = 20;
 	int graphPixelWidth = 1920;
 	int graphPixelHeight = 1080;
-	
+
 	public GraphDisplay(ArrayList<Function> function) {
 		this.function = function;
 		this.setBounds(0, 0, graphPixelWidth, graphPixelWidth);
-		//this.setBackground(Color.BLACK);
+		// this.setBackground(Color.BLACK);
 	}
-	
+
 	public GraphDisplay(ArrayList<Function> function, int lowerBound, int upperBound, int functionF, int functionG) {
 		toDrawArea = true;
 		this.function = function;
@@ -40,73 +40,85 @@ public class GraphDisplay extends JPanel {
 		this.functionGIndex = functionG;
 		this.setBounds(0, 0, graphPixelWidth, graphPixelWidth);
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
-		if(toDrawArea) {
+		if (toDrawArea) {
 			drawArea(function.get(functionFIndex), function.get(functionGIndex), g);
 		}
-		for(Function f: function) {
+		for (Function f : function) {
 			drawFunction(f, g);
 		}
- 	}
-	
+	}
+
 	public void drawArea(Function f, Function g, Graphics graph) {
 		Integer lastX = null;
 		Integer lastYG = null;
 		Integer lastYF = null;
 		double interval = new Double(graphWidth) * new Double(upperBound - lowerBound) / new Double(graphPixelWidth);
-		for(double x = lowerBound; x < (upperBound); x += interval) {
+		for (double x = lowerBound; x < (upperBound); x += interval) {
 			double yCordF = f.getValueAt(x);
 			yCordF *= -1;
 			yCordF *= new Double(graphPixelHeight);
 			yCordF /= new Double(graphHeight);
-			yCordF += graphPixelHeight /  2;
+			yCordF += graphPixelHeight / 2;
 			double yCordG = g.getValueAt(x);
 			yCordG *= -1;
 			yCordG *= new Double(graphPixelHeight);
 			yCordG /= new Double(graphHeight);
-			yCordG += graphPixelHeight /  2;
+			yCordG += graphPixelHeight / 2;
 			double shift = (new Double(startX));
 			double xCord = (x - shift) * new Double(graphPixelWidth);
 			xCord /= new Double(graphWidth);
-			if(lastX != null && lastYF != null) {
-				Polygon toDraw = new Polygon();
-				toDraw.addPoint(lastX, lastYF);
-				toDraw.addPoint(lastX, lastYG);
-				toDraw.addPoint((int)xCord, (int)yCordG);
-				toDraw.addPoint((int)xCord, (int)yCordF);
-				graph.setColor(Rainbow.getNextColor());
-				graph.fillPolygon(toDraw);
+			if (!new Double(yCordF).toString().contains("Infinity") && !new Double(yCordF).toString().equals("NaN")
+					&& yCordF != Double.NEGATIVE_INFINITY) {
+				if (lastX != null && lastYF != null) {
+					Polygon toDraw = new Polygon();
+					toDraw.addPoint(lastX, lastYF);
+					toDraw.addPoint(lastX, lastYG);
+					toDraw.addPoint((int) xCord, (int) yCordG);
+					toDraw.addPoint((int) xCord, (int) yCordF);
+					graph.setColor(Rainbow.getNextColor());
+					graph.fillPolygon(toDraw);
+				}
 			}
 			lastX = (int) xCord;
-			lastYF = (int) yCordF;
-			lastYG = (int) yCordG;
+			if(!Double.toString(yCordF).equals("NaN")) {
+				lastYF = (int)yCordF;
+			}			
+			if(!Double.toString(yCordG).equals("NaN")) {
+				lastYG = (int)yCordG;
+			}	
 		}
 	}
-	
+
 	public void drawFunction(Function f, Graphics graph1) {
-		Graphics2D graph = (Graphics2D)graph1;
+		Graphics2D graph = (Graphics2D) graph1;
 		graph.setColor(Rainbow.getRandom());
 		graph.setStroke(new BasicStroke(5));
 		Integer lastX = null;
 		Integer lastY = null;
 		double interval = new Double(graphWidth) / new Double(graphPixelWidth);
-		for(double x = startX; x < (startX + graphWidth); x += interval) {
-			double yCord = f.getValueAt(x);
+		for (double x = startX; x < (startX + graphWidth); x += interval) {
+			Double yCord = f.getValueAt(x);
 			yCord *= -1;
 			yCord *= new Double(graphPixelHeight);
 			yCord /= new Double(graphHeight);
-			yCord += graphPixelHeight /  2;
+			yCord += graphPixelHeight / 2;
 			double shift = (new Double(startX));
 			double xCord = (x - shift) * new Double(graphPixelWidth);
 			xCord /= new Double(graphWidth);
-			if(lastX != null && lastY != null) {
-				graph.drawLine(lastX, lastY, (int)xCord, (int)yCord);
-			}
+			if (lastX != null && lastY != null && !yCord.toString().equals("NaN")
+					&& Double.isFinite(yCord)) {
+				graph.drawLine(lastX, lastY, (int) xCord, yCord.intValue());
+				System.out.println(lastY + " " + yCord);
+				}
 			lastX = (int) xCord;
-			lastY = (int) yCord;
+			if(!yCord.toString().equals("NaN")) {
+				lastY = yCord.intValue();
+			}
+			
 		}
 	}
-	
+
 }
